e53cb70d9929a314463671101fdc55776c28c4ca
Oscar <97081587@st.deltion.nl>
2023-12-19 10:30:21 +0100

    huh

--- app/Http/Controllers/FormulierController.php
+++ app/Http/Controllers/FormulierController.php
@@ -2,21 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FormulierController extends Controller
 {
      public function form(Request $request) {
-        //dd($request);
-         $form = $request->validate([
+         //dd($request);
+         $validatedData = $request->validate([
             'naam' => 'required',
             'adres' => 'required',
             'postcode' => ['required','max:12'],
             'plaats' => ['required','max:85'],
             'Bdatum' => 'required',
          ]);
+         dd($request);
+         $form = new User();
+
+         $form->naam = $validatedData['naam'];
+         $form->adres = $validatedData['adres'];
+         $form->postcode = $validatedData['postcode'];
+         $form->plaats = $validatedData['plaats'];
+         $form->Bdatum = $validatedData['Bdatum'];
 
          $Form->save();
+
          return view ('besteld');
      }
 }
