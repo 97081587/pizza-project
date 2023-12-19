a32153709877ab1f4d872f4d4f0ccb6f757f83e1
Oscar <97081587@st.deltion.nl>
2023-12-19 11:26:36 +0100

    debugger

--- app/Http/Controllers/FormulierController.php
+++ app/Http/Controllers/FormulierController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class FormulierController extends Controller
 {
      public function form(Request $request) {
-         //dd($request);
+         dd($request);
          $validatedData = $request->validate([
             'naam' => 'required',
             'adres' => 'required',
--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class PizzaController extends Controller
 {
     public function pizza(Request $request) {
-        dd($request);
+        //dd($request);
         $validatedData = $request->validate([
             'HawaiiList' => 'required',
             'FunghiList' => 'required',
@@ -18,7 +18,6 @@ class PizzaController extends Controller
             'Kosten' => 'required',
             'BOA' => 'required'
         ]);
-        
         $pizza = new Pizza();
 
         $pizza->HawaiiList = $validatedData['HawaiiList'];
