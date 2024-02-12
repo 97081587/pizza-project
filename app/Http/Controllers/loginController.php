1f07db43e47e3e2e4184b1114ae619777a1b30f2
Oscar <97081587@st.deltion.nl>
2024-02-12 09:47:19 +0100

    controller begin

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -4,7 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class login extends Controller
+class loginController extends Controller
 {
-    //
+    public function form(Request $request) {
+        //dd($request);
+        $validatedData = $request->validate([
+           'gebruikersnaam' => 'required',
+           'Wachtwoord' => 'required',
+        ]);
+       // dd($request);
+        $form = new User();
+
+        $form->Naam = $request['naam'];
+        $form->Adres = $request['adres'];
+        //dd($request);
+        $form->save();
+        //dd($request);
+        return view('home');
+    }
 }
