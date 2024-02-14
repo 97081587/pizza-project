e83167f896a7abe4d6febb29c3917ada74c207d4
Oscar <97081587@st.deltion.nl>
2024-02-14 11:34:49 +0100

    split

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id('User');
             $table->string('Email');
-            $table->string('Wachtwoord');;
+            $table->string('Wachtwoord');
             $table->rememberToken();
             $table->timestamps();
         });
