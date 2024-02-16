942083b5d0c102d16019cd76b4e723cb01b0e7ea
Oscar <97081587@st.deltion.nl>
2024-02-16 13:51:24 +0100

    !

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('email')->unique()->default('');
+            $table->timestamp('email_verified_at')->default('');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
