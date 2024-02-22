c0df52f1e21d0c18f9cdac412ae1a1939369069e
Oscar <97081587@st.deltion.nl>
2024-02-22 13:03:55 +0100

    inlog werkt weer & css besteld gedaan

--- resources/views/besteld.blade.php
+++ resources/views/besteld.blade.php
@@ -3,28 +3,30 @@
 @section('title','Dit heeft u besteld')
 
 @section('content')
-<div>
+<div class="gegevens">
     <h2>Dit heeft u besteld:</h2>
     @foreach ($pizzas as $pizza)
-        <p>{{$pizza->HawaiiList}}  Stuks Pizza Hawaii 🍍🍕</p>
-        <p>{{$pizza->FunghiList}} Stuks Pizza Funghi 🍄🍕</p>
-        <p>{{$pizza->MargheritaList}} Stuks Pizza Margherita 🌿🍕</p>
-        <p>{{$pizza->MarinaList}} Stuks Pizza Marina 🐟🍕</p>
-        <p>{{$pizza->QFormaggiList}} Stuks Pizza Quattro Formaggi 🧀🍕</p>
-        <p>Totaalprijs: €{{$pizza->Totaalprijs}},-</p>
-        <p>Bestellen of afhalen: {{$pizza->BOA}}<p>
+        <div>
+            <p>{{$pizza->HawaiiList}}  Stuks Pizza Hawaii 🍍🍕</p>
+            <p>{{$pizza->FunghiList}} Stuks Pizza Funghi 🍄🍕</p>
+            <p>{{$pizza->MargheritaList}} Stuks Pizza Margherita 🌿🍕</p>
+            <p>{{$pizza->MarinaList}} Stuks Pizza Marina 🐟🍕</p>
+            <p>{{$pizza->QFormaggiList}} Stuks Pizza Quattro Formaggi 🧀🍕</p>
+            <p>Totaalprijs: €{{$pizza->Totaalprijs}},-</p>
+            <p>Bestellen of afhalen: {{$pizza->BOA}}<p>
     @endforeach  
         @foreach ($gegevens as $gegeven)  
             <p>Besteldatum: {{$gegeven->Bdatum}}<p>
-        <div class="gegevens">        
-            <h2>Uw gegevens:</h2>
-            <p>Adres: {{$gegeven->Adres}}<p>
-            <p>Postcode: {{$gegeven->Postcode}}<p>
-            <p>Plaats: {{$gegeven->Plaats}}<p>
+        </div>
+            <div>        
+                <h2>Uw gegevens:</h2>
+                <p>Adres: {{$gegeven->Adres}}<p>
+                <p>Postcode: {{$gegeven->Postcode}}<p>
+                <p>Plaats: {{$gegeven->Plaats}}<p>
         @endforeach
     @foreach ($Users as $User)
-        <p>E-mailadres: {{$User ->email}}<p>
-        </div>
+                <p>E-mailadres: {{$User ->email}}<p>
+            </div>
     @endforeach
 </div>
 @endsection
