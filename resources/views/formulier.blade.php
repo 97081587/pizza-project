35561185d4c5cbf62a21048c0a6fc2b1988e53e5
Oscar <97081587@st.deltion.nl>
2024-02-28 11:27:13 +0100

    dropdown log in

--- resources/views/formulier.blade.php
+++ resources/views/formulier.blade.php
@@ -52,56 +52,7 @@ if(isset($_POST['submit'])){
       <button>Voltooien</button>
     </form>
   @else
-      <div class=formulierRegistrerenEnInloggen>
-        <div class="1">
-          <form method='POST' action="/registreren">
-            @csrf
-            <h2>Registreren</h2>
-            <div>
-              <label for="email">E-mailadres:</label>
-              <br>
-              <input type="text" name="RegiEmail" placeholder="E-mailadres">
-              <br>
-            </div>
-              <div>
-                <label for="password">Wachtwoord:</label>
-                <br>
-                <input type="password" name="RegiPassword" placeholder="Wachtwoord">
-                <br>
-              </div>
-              {{-- <div>
-                <label for="password">Bevestig uw achtwoord:</label>
-                <br>
-                <input type="password" name="RegiPassword"
-                    placeholder="Bevestig uw wachtwoord">
-                <br>
-              </div> --}}
-            <br>
-            <button>Registreren</button>
-          </form>
-        </div>
-        <br>
-      <div class="2">      
-        <form method='POST' action="/ingelogd">
-          @csrf
-          <h2>Inloggen</h2>
-            <div>
-              <label for="email">E-mailadres:</label>
-              <br>
-              <input type="text" name="email" placeholder="E-mailadres">
-              <br>
-            </div>
-          <div>
-            <label for="password">Wachtwoord:</label>
-            <br>
-            <input type="password" name="password" placeholder="Wachtwoord">
-            <br>
-          </div>
-          <br>
-            <button>Inloggen</button>
-        </form>
-      </div>
-    </div>
+    
   @endauth
 </div>
 @endsection
