7e064104a8f826bd3a58eeebd49a5d2f2e597852
Oscar <97081587@st.deltion.nl>
2024-03-14 11:43:41 +0100

    prijs moet geupdated worden

--- resources/views/formulier.blade.php
+++ resources/views/formulier.blade.php
@@ -6,16 +6,16 @@
 $adres = '';
 $Pcode = '';
 $plaats = '';
-$Bdatum = '';
-$Totaalprijs ='';
-//$newDate = date ('l', strtotime($Bdatum));
+// $Bdatum = '';
+// $Totaalprijs ='';
+// $newDate = date ('l', strtotime($Bdatum));
 
 // dit gaat naar de DB
 if(isset($_POST['submit'])){
     $adres = $_POST['adres'];
     $postcode = $_POST['postcode'];
     $plaats = $_POST['plaats'];
-    $Bdatum = $_POST['Bdatum'];
+    // $Bdatum = $_POST['Bdatum'];
     $Totaalprijs = $_POST['Totaalprijs'];
 }
 ?>
@@ -50,18 +50,21 @@ if(isset($_POST['submit'])){
       <div name="Bdatum">
             <label for="Bdatum">Bestel/afhaal datum:</label>
             <br>
-            <input type="date" id="Bdatum" name="Bdatum"  min="<?php echo date("Y-m-d"); ?>" required>
-            <?php
-              if ($Bdatum == 1||'Monday') {
-                  //maandagprijs();
+            <input type="date" id="Bdatum" name="Bdatum" oninput="KortingMAVR()"  min="<?php echo date("Y-m-d"); ?>" required>
+            <script>
+              function KortingMAVR() {
+                var Bdatum = document.GetElementById('Bdatum');
+                var Bdatum2 = new Date(Bdatum.value);
+
+                // if (Bdatum2.getDay() === 1) {
+
+                // }
+
+                if (Bdatum2.getDay() === 5 && $Totaalprijs > 20) {
+                  Totaalprijs = $Totaalprijs - 15 * ($Totaalprijs / 100);
+                }
               }
-            
-             if (($Bdatum == 4||'Friday') && $Totaalprijs > 20) {
-                  $Totaalprijs = $Totaalprijs - 15 * ($Totaalprijs / 100);
-                  echo "<script>alert($Bdatum);</script>";
-              echo "<script>alert($Totaalprijs);</script>";
-              }
-            ?>
+             </script>
       </div>
       <br>
       <button class ="voltooi">Voltooien</button>
